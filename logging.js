7b5d25d22bde1c264eab2174ab93f60a056be4f6
n1clude <56724583+n1clud3@users.noreply.github.com>
2024-04-16 09:09:10 +0300

    Allow logging methods to pass multiple args

--- logging.js
+++ logging.js
@@ -1,20 +1,28 @@
 module.exports = class Logger {  
   /**
    * Message to send to console
-   * @param {any} msg
+   * @param {...any} msg
    */
-  static log(msg) {
+  static log(...msg) {
     const timestamp = new Date();
-    console.log(`[INFO][UTC${timestamp.getUTCHours()}:${timestamp.getUTCMinutes()}:${timestamp.getUTCSeconds()}] ${msg}`);
+    console.log(`[INFO][UTC${timestamp.getUTCHours()}:${timestamp.getUTCMinutes()}:${timestamp.getUTCSeconds()}]`, ...msg);
   }
 
-  static warn(msg) {
+  /**
+   * Warning to send to console
+   * @param {...any} msg
+   */
+  static warn(...msg) {
     const timestamp = new Date();
-    console.warn(`[WARN][UTC${timestamp.getUTCHours()}:${timestamp.getUTCMinutes()}:${timestamp.getUTCSeconds()}] ${msg}`);
+    console.warn(`[WARN][UTC${timestamp.getUTCHours()}:${timestamp.getUTCMinutes()}:${timestamp.getUTCSeconds()}]`, ...msg);
   }
 
-  static error(msg) {
+  /**
+   * Error to send to console
+   * @param {...any} msg 
+   */
+  static error(...msg) {
     const timestamp = new Date();
-    console.error(`[ERROR][UTC${timestamp.getUTCHours()}:${timestamp.getUTCMinutes()}:${timestamp.getUTCSeconds()}] ${msg}`);
+    console.error(`[ERROR][UTC${timestamp.getUTCHours()}:${timestamp.getUTCMinutes()}:${timestamp.getUTCSeconds()}]`, ...msg);
   }
 }
