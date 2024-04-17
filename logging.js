9f4d7bd9daff631d229b714dd221f7b2109eef79
n1clude <56724583+n1clud3@users.noreply.github.com>
2024-04-17 17:29:52 +0300

    Do a leftpad for a timestamp in logger

--- logging.js
+++ logging.js
@@ -1,28 +1,41 @@
-module.exports = class Logger {  
-  /**
+//@ts-check
+
+/**
+ * @param {Date} timestamp 
+ * @returns {string} formatted UTC string
+ */
+function generatePaddedTimestamp(timestamp) {
+  const hours = timestamp.getUTCHours().toString().padStart(2, "0");
+  const minutes = timestamp.getUTCMinutes().toString().padStart(2, "0");
+  const seconds = timestamp.getUTCSeconds().toString().padStart(2, "0");
+  return hours + ":" + minutes + ":" + seconds
+}
+
+/**
    * Message to send to console
    * @param {...any} msg
    */
-  static log(...msg) {
-    const timestamp = new Date();
-    console.log(`[INFO][UTC${timestamp.getUTCHours()}:${timestamp.getUTCMinutes()}:${timestamp.getUTCSeconds()}]`, ...msg);
-  }
-
-  /**
-   * Warning to send to console
-   * @param {...any} msg
-   */
-  static warn(...msg) {
-    const timestamp = new Date();
-    console.warn(`[WARN][UTC${timestamp.getUTCHours()}:${timestamp.getUTCMinutes()}:${timestamp.getUTCSeconds()}]`, ...msg);
-  }
-
-  /**
-   * Error to send to console
-   * @param {...any} msg 
-   */
-  static error(...msg) {
-    const timestamp = new Date();
-    console.error(`[ERROR][UTC${timestamp.getUTCHours()}:${timestamp.getUTCMinutes()}:${timestamp.getUTCSeconds()}]`, ...msg);
-  }
+function log(...msg) {
+  const timestamp = generatePaddedTimestamp(new Date());
+  console.log(`[INFO][UTC${timestamp}]`, ...msg);
 }
+
+/**
+ * Warning to send to console
+ * @param {...any} msg
+ */
+function warn(...msg) {
+  const timestamp = generatePaddedTimestamp(new Date());
+  console.log(`[WARN][UTC${timestamp}]`, ...msg);
+}
+
+/**
+ * Error to send to console
+ * @param {...any} msg 
+ */
+function error(...msg) {
+  const timestamp = generatePaddedTimestamp(new Date());
+  console.log(`[ERROR][UTC${timestamp}]`, ...msg);
+}
+
+module.exports = {log, warn, error}
