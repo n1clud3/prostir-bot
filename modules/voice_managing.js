5a4a86da6748d14af952030a4720aa86dbd3b2ff
n1clude <56724583+n1clud3@users.noreply.github.com>
2024-04-18 19:32:11 +0300

    Add .catch() in places prone to error.

--- modules/voice_managing.js
+++ modules/voice_managing.js
@@ -12,7 +12,9 @@ module.exports = function initModule(client) {
   
   client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
     if (oldState.channel !== null && oldState.channel.id !== null && channels.includes(oldState.channel.id) && oldState.channel.members.size <= 0) {
-      oldState.channel.delete();
+      oldState.channel.delete().catch((reason) => {
+        console.error("Channel deletion was rejected. Reason:", reason);
+      });
       delete channels[channels.indexOf(oldState.channel.id)];
       logger.log(`Deleted voice channel "${oldState.channel.name}" (${oldState.channel.id})`);
     }
@@ -55,8 +57,12 @@ module.exports = function initModule(client) {
       if (newState.member === null) {return};
       logger.log(`Moved ${newState.member.displayName} (${newState.member.id}) to a new voice channel`);
       //@ts-ignore
-      newState.member.voice.setChannel(channel);
+      newState.member.voice.setChannel(channel).catch((reason) => {
+        logger.error("Move member action was rejected. Reason:", reason);
+      });
       channels.push(channel.id);
+    }).catch((reason) => {
+      logger.error("Channel creation was rejected. Reason:", reason);
     })
   });
   logger.log("Voice managing module is set up.");
