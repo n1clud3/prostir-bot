1d8c6038f789e9efcbff8c33a9e88b45ff99ba6a
n1clude <56724583+n1clud3@users.noreply.github.com>
2024-04-18 21:08:52 +0300

    Simplify the permission grant

--- modules/voice_managing.js
+++ modules/voice_managing.js
@@ -18,48 +18,25 @@ module.exports = function initModule(client) {
       delete channels[channels.indexOf(oldState.channel.id)];
       logger.log(`Deleted voice channel "${oldState.channel.name}" (${oldState.channel.id})`);
     }
-  
     
     if (newState.channel === null || !config.modules.voice_managing.master_voices.includes(newState.channel.id) || newState.member === null || client.user === null) {return};
     logger.log(`${newState.member.displayName} joined a master voice "${newState.channel.name}" (${newState.channelId})`);
-  
-    const botUser = newState.guild.roles.botRoleFor(client.user);
-    if (botUser === null) {return};
-    
-    const channelPerms = [
-      {
-        "id": newState.member.id,
-        "allow": [PermissionsBitField.Flags.ManageChannels],
-      },
-      {
-        "id": botUser.id,
-        "allow": [PermissionsBitField.Flags.ViewChannel],
-      },
-      {
-        "id": newState.guild.roles.everyone,
-        "deny": [PermissionsBitField.Flags.ViewChannel],
-        
-      }
-    ];
-    const categoryVisible = newState.channel.parent?.permissionsFor(newState.guild.roles.everyone).toArray().includes("ViewChannel");
-  
-    if (categoryVisible) {
-      channelPerms.pop();
-    }
   
     newState.guild.channels.create({
       "name": newState.member.displayName,
       "parent": newState.channel.parent,
       "type": ChannelType.GuildVoice,
       "userLimit": 0,
-      "permissionOverwrites": channelPerms,
     }).then((channel) => {
       if (newState.member === null) {return};
-      logger.log(`Moved ${newState.member.displayName} (${newState.member.id}) to a new voice channel`);
+      channel.permissionOverwrites.edit(newState.member, { ManageChannels: true }).catch((reason) => {
+        logger.error("Permission overwrite was rejected. Reason:", reason);
+      });
       //@ts-ignore
       newState.member.voice.setChannel(channel).catch((reason) => {
         logger.error("Move member action was rejected. Reason:", reason);
       });
+      logger.log(`Moved ${newState.member.displayName} (${newState.member.id}) to a new voice channel`);
       channels.push(channel.id);
     }).catch((reason) => {
       logger.error("Channel creation was rejected. Reason:", reason);
