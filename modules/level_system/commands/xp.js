fcb3863208be016133d2c60fe0a6f424d252947b
n1clude <56724583+n1clud3@users.noreply.github.com>
2024-04-18 22:32:55 +0300

    Implement response if the module is disabled.

--- modules/level_system/commands/xp.js
+++ modules/level_system/commands/xp.js
@@ -3,6 +3,7 @@
 const { SlashCommandBuilder, SlashCommandUserOption } = require("discord.js");
 const sqlite3 = require("sqlite3").verbose();
 const logger = require("./../../../logging");
+const config = require("./../../../config.json");
 const { calculateLevel } = require("./../.");
 
 module.exports = {
@@ -10,6 +11,12 @@ module.exports = {
     .setName("xp")
     .setDescription("Get the XP amount."),
   async execute(/** @type {import("discord.js").Interaction<import("discord.js").CacheType>}*/interaction) {
+    if (!config.modules.level_system.enabled) {
+      //@ts-ignore
+      interaction.reply("XP модуль наразі вимкнений.");
+      return;
+    };
+    
     const bot_db = new sqlite3.Database("bot.db");
     bot_db.get("SELECT xp FROM levels_data WHERE uid = (?)", interaction.user.id, async (err, row) => {
       if (err) {
