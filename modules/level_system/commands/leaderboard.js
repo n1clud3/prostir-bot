3c8c855d9902db5cc58e6efb66f82ba57a4332f9
n1clude <56724583+n1clud3@users.noreply.github.com>
2024-04-19 10:33:55 +0300

    Create leaderboard command. Probably WIP

---
+++ modules/level_system/commands/leaderboard.js
@@ -0,0 +1,68 @@
+//@ts-check
+
+const sqlite3 = require("sqlite3").verbose();
+const config = require("./../../../config.json");
+const logger = require("./../../../logging");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { execute } = require("./xp");
+const { calculateLevel } = require("..");
+
+function sortObjectByValue(obj) {
+  const sortedEntries = Object.entries(obj)
+      .sort((a, b) => b[1] - a[1]); // Sorting entries based on numeric values
+
+  const sortedObj = {};
+  for (const [key, value] of sortedEntries) {
+      sortedObj[key] = value;
+  }
+
+  return sortedObj;
+}
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("leaderboard")
+    .setDescription("Показує топ 10 людей по кількості XP"),
+  async execute(/** @type {import("discord.js").Interaction<import("discord.js").CacheType>}*/interaction) {
+    if (!config.modules.level_system.enabled) {
+      //@ts-ignore
+      interaction.reply(":stop_sign: XP модуль наразі вимкнений.");
+      return;
+    };
+
+    const bot_db = new sqlite3.Database("bot.db");
+    bot_db.all("SELECT * FROM levels_data", async (err, rows) => {
+      if (err) {
+        logger.error("DB", err);
+        //@ts-ignore
+        await interaction.reply(":stop_sign: Виникла проблема при виконанні команди.");
+        return;
+      }
+
+      if (!rows) {
+        //@ts-ignore
+        await interaction.reply(":warning: Таблиця лідерів пуста.");
+        return;
+      }
+
+      const leaderboard = {}
+      for (const row in rows) {
+        leaderboard[rows[row].uid] = rows[row].xp;
+      }
+
+      let message = "## :bar_chart: Дошка лідерів\n";
+
+      let i = 1;
+      
+      for (const uid in sortObjectByValue(leaderboard)) {
+        if (i >= 10) break;
+        const username = (await interaction.client.users.fetch(uid)).username;
+        //message.push({name: username, value: leaderboard[uid].toString(), inline: true})
+        message = message.concat(`\n\`${i}. ${username} - ${leaderboard[uid].toString()} XP - LVL ${calculateLevel(leaderboard[uid]).toString()}\``);
+        i++;
+      }
+      //@ts-ignore
+      await interaction.reply(message);
+    })
+  }
+}
