92b62d8d3031d8e2378c76460a9b568a3df39356
Mykola Malovanyi <56724583+n1clud3@users.noreply.github.com>
2024-08-29 11:56:30 +0200

    Add ping_response module

---
+++ modules/ping_response/index.js
@@ -0,0 +1,29 @@
+const { Client, Events } = require("discord.js");
+const logger = require("../../logging");
+const config = require("../../config.json");
+const _ = require("lodash");
+
+let responses_left = [];
+
+/**
+ * @param {Client} client
+ */
+function initModule(client) {
+  client.on(Events.MessageCreate, async (msg) => {
+    if (!msg.mentions.has(client.user)) return;
+
+    if (responses_left.length < 1) responses_left = config.modules.ping_response.responses;
+
+    const selection = _.random(0, responses_left.length - 1, false);
+    const selected_response = responses_left[selection];
+    // remove selected response from left responses
+    responses_left = responses_left.slice(0, selection).concat(responses_left.slice(selection + 1));
+    logger.trace(responses_left);
+
+    await msg.reply(selected_response);
+  });
+
+  logger.log("Ping responses are set up.");
+}
+
+module.exports = { initModule };
